8e70405d8f848aa079a7b5511e44fac1b52680e2
umer <bscs23211@itu.edu.pk>
2024-09-10 00:12:59 +0500

    Tweaking

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ mod parser;
 use parser::Comms;
 use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
-    //let path = env::current_dir()?;
     loop {
-        // print!("{} $ ", path.display());
-        // stdout().flush().unwrap();
         builtins::shell_prompt();
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
