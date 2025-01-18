9a7388da82211869419de4f61cb681a8ec015032
umer <bscs23211@itu.edu.pk>
2025-01-19 00:32:09 +0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::io::stdin;
+use std::{io::stdin, process::exit};
 mod builtins;
 mod parser;
 use parser::Comms;
 use std::error::Error;
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut home_dir: String = match Comms::init_home_dir() {
+        Ok(dir) => dir,
+        Err(_) => exit(1),
+    };
+
     loop {
-        builtins::shell_prompt();
+        builtins::shell_prompt(&home_dir);
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
-        let user_comms = Comms::tokenize(input);
-        builtins::comms_process(&user_comms);
+        let user_comms = Comms::tokenize(input, home_dir.clone());
+        builtins::comms_process(&user_comms, &mut home_dir);
     }
 }
