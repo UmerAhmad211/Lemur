4e64b69310c65ad4ab865fcb9646eb0f390574cc
umer <bscs23211@itu.edu.pk>
2025-01-19 00:32:47 +0500

    chore

--- src/parser.rs
+++ src/parser.rs
@@ -1,17 +1,28 @@
+use std::env;
+
 pub struct Comms {
     pub key_word: String,
     pub args: Vec<String>,
+    pub curr_dir: String,
 }
 
 impl Comms {
-    pub fn tokenize(input: String) -> Comms {
+    pub fn tokenize(input: String, _curr_dir: String) -> Comms {
         let tokenized_in: Vec<String> =
             input.trim().split(' ').map(|val| val.to_string()).collect();
         let user_comms = Comms {
             key_word: tokenized_in[0].clone(),
             args: tokenized_in[1..].to_vec(),
+            curr_dir: _curr_dir,
         };
 
         user_comms
+    }
+
+    pub fn init_home_dir() -> Result<String, ()> {
+        match env::var("HOME") {
+            Ok(path) => Ok(path),
+            Err(_) => Err(()),
+        }
     }
 }
